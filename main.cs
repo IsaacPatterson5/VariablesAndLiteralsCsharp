bc176ba062080b8e3a9e8fcd6485281fb3976e10
Isaac Patterson <85522516+IsaacPatterson5@users.noreply.github.com>
2022-02-07 23:07:58 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,16 @@
+using System;
+
+class Program {
+  public static void Main (string[] args) {
+
+    string name = "Isaac";
+      Console.Write("Hello, " + "my name is " + name + ". "); 
+
+    string teamates = "Jonathan, Quincy, and Vijay";
+      Console.Write("My teamates are, " + teamates + ". ");
+
+      string CustomizedMessage = "I would like to say that I will be looking forward to working with ";
+      Console.Write(CustomizedMessage + teamates + " in the future. ");  
+
+  }
+}
